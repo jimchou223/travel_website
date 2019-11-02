fff54bd908e3bd9aff46b20bef8b6da9aa0e15c6
jimchou223 <ethan790223@gmail.com>
2019-11-01 21:38:36 -0700

    update

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+const spotBtn = document.getElementById('spot-btn')
+const foodBtn = document.getElementById('food-btn')
+const spotHidden = document.querySelectorAll('.spot-hidden')
+const foodHidden = document.querySelectorAll('.food-hidden')
+
+let spotStatus = true
+let foodStatus = true
+
+
+// mobile mode navbar control
+spotBtn.addEventListener('click', () => {
+    if (window.innerWidth < 768 && spotStatus == true) {
+        spotHidden.forEach((btn) => btn.classList.remove('hidden'))
+        spotStatus = false
+    } else if (window.innerWidth < 768 && spotStatus == false) {
+        spotHidden.forEach((btn) => btn.classList.add('hidden'))
+        spotStatus = true
+        
+    }
+})
+foodBtn.addEventListener('click', () => {
+    if (window.innerWidth < 768 && foodStatus == true) {
+        foodHidden.forEach((btn) => btn.classList.remove('hidden'))
+        foodStatus = false
+    } else if (window.innerWidth < 768 && foodStatus == false) {
+        foodHidden.forEach((btn) => btn.classList.add('hidden'))
+        foodStatus = true
+    }
+})
+
+// score range control
+
+const score = document.getElementById('score')
+const output = document.getElementById('output')
+score.addEventListener('change', ()=> {
+    // output.innerHTML = '';
+    output.innerHTML = score.value;
+})
